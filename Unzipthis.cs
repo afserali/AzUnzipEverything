000d4e4eb09ebb07084ab3c82250ed7788d8703b
Afser Ali <afser-ali@lntecc.com>
2020-12-26 12:54:50 +0530

    Update Unzipthis.cs

--- Unzipthis.cs
+++ Unzipthis.cs
@@ -36,7 +36,7 @@ namespace AzUnzipEverything
                         {
                             foreach (ZipArchiveEntry entry in archive.Entries)
                             {
-                                log.LogInformation($"Now processing {entry.FullName}");
+                                log.LogInformation($"-Afser Now processing {entry.FullName}");
 
                                 //Replace all NO digits, letters, or "-" by a "-" Azure storage is specific on valid characters
                                 string valideName = Regex.Replace(entry.Name,@"[^a-zA-Z0-9\-]","-").ToLower();
